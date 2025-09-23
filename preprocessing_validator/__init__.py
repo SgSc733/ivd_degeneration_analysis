ae7dbf626c6ccce749dce8e9e2a20cc2305614e6
SGSC <147935217+SgSc733@users.noreply.github.com>
2025-09-23 20:58:37 +0800

    Delete preprocessing_validator directory

--- preprocessing_validator/__init__.py
+++
@@ -1,9 +0,0 @@
-from .preprocessing_validator import PreprocessingValidator
-
-__version__ = '1.0.0'
-__author__ = 'Your Name'
-__all__ = ['PreprocessingValidator']
-
-def main():
-    from .preprocessing_validator import main as validator_main
-    validator_main()
